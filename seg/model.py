891201097e0a0b0557b8f9c0658628358da5b2f7
Benjamin Kiessling <mittagessen@l.unchti.me>
2018-09-17 17:20:13 +0200

    gix reversion error

--- seg/model.py
+++ seg/model.py
@@ -138,12 +138,12 @@ class UnetDecoder(nn.Module):
         return F.relu(self.deconv(x, output_size=output_size))
 
 
-class ResSkipNet(nn.Module):
+class ResUNet(nn.Module):
     """
     ResNet-34 encoder + U-Net decoder
     """
     def __init__(self, cls=1, refine_encoder=False):
-        super(ResSkipNet, self).__init__()
+        super(ResUNet, self).__init__()
         self.cls = cls
         # squeezenet feature extractor
         self.resnet = models.resnet34(pretrained=True)
